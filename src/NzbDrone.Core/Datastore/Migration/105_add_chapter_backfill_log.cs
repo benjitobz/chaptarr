5f2b64fd46ed0f14062e788216aafe632e16277d
benjitobz <benjitobz@users.noreply.github.com>
2026-09-03 15:57:49 -0400

    Renumber the chapter backfill migration to 105

--- src/NzbDrone.Core/Datastore/Migration/105_add_chapter_backfill_log.cs
+++ src/NzbDrone.Core/Datastore/Migration/105_add_chapter_backfill_log.cs
@@ -3,7 +3,7 @@ using NzbDrone.Core.Datastore.Migration.Framework;
 
 namespace NzbDrone.Core.Datastore.Migration
 {
-    [Migration(104)]
+    [Migration(105)]
     public class add_chapter_backfill_log : NzbDroneMigrationBase
     {
         protected override void MainDbUpgrade()
